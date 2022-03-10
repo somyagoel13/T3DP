e8bb057c91a072548286f7ca8ed7f70727ae094d
Somya Goel <37614599+somyagoel13@users.noreply.github.com>
2022-03-10 18:03:05 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -71,8 +71,7 @@ class FrameExtractor():
     def extract_frames(self, every_x_frame, img_name, dest_path=None, img_ext = '.jpg', frames=[200,300]):
         if not self.vid_cap.isOpened():
             self.vid_cap = cv2.VideoCapture(self.video_path)
-        else:
-            print("vid cap error")
+        
         if dest_path is None:
             dest_path = os.getcwd()
         else:
@@ -80,15 +79,12 @@ class FrameExtractor():
                 os.mkdir(dest_path); print(f'Created the following directory: {dest_path}')
         
         frame_cnt = 0; img_cnt = 0
-        print("self.vid_cap.isOpened() : ",self.vid_cap.isOpened())
         while self.vid_cap.isOpened():
             success,image = self.vid_cap.read() 
-            print("success :",success)
             if not success: break
             if frame_cnt % every_x_frame == 0 and (frame_cnt<frames[1] and frame_cnt>frames[0]):
                 img_path = os.path.join(dest_path, ''.join([img_name,  '%06d' % (img_cnt+1), img_ext]))
                 cv2.imwrite(img_path, image)  
-                print("extract frame path img ",img_path)
                 img_cnt += 1
             frame_cnt += 1
             if(frame_cnt>frames[1]): break
