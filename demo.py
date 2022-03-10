a967cf64f1fd3aaf83ff4d00a20658a329498ea2
Somya Goel <37614599+somyagoel13@users.noreply.github.com>
2022-03-10 13:13:32 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -301,10 +301,10 @@ def run_hmar(video_path):
                  
     return time_  
             
-            
+import requests            
 if __name__ == '__main__':
 
-    list_ = ['xEH_5T9jMVU']
+    list_ = ['1']
     
     for i, YOUTUBE_ID in enumerate(list_):
         video_folder   = "video_"+ str(i) +"/"
@@ -315,13 +315,49 @@ if __name__ == '__main__':
         os.system("mkdir _DATA/DEMO/"+video_folder)
         os.system("mkdir _DATA/DEMO/"+video_folder+"/detections")
 
-        video = YouTube('https://www.youtube.com/watch?v=' + YOUTUBE_ID)
-        print('Summary:')
-        print(f'Title: {video.title}')
-        print(f'Duration: {video.length / 60:.2f} minutes')
-        print(f'# of views: {video.views}')
-        print(video.streams.all())
-        video.streams.get_by_itag(18).download(output_path = dataset_path + video_folder, filename="youtube.mp4")
+        #video = YouTube('https://www.youtube.com/watch?v=' + YOUTUBE_ID)
+        #print('Summary:')
+        #print(f'Title: {video.title}')
+        #print(f'Duration: {video.length / 60:.2f} minutes')
+        #print(f'# of views: {video.views}')
+        #print(video.streams.all())
+        
+        #taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
+        
+
+        def download_file_from_google_drive(id, destination):
+            URL = "https://docs.google.com/uc?export=download"
+
+            session = requests.Session()
+
+            response = session.get(URL, params = { 'id' : id }, stream = True)
+            token = get_confirm_token(response)
+
+            if token:
+                params = { 'id' : id, 'confirm' : token }
+                response = session.get(URL, params = params, stream = True)
+
+            save_response_content(response, destination)    
+
+        def get_confirm_token(response):
+            for key, value in response.cookies.items():
+                if key.startswith('download_warning'):
+                    return value
+
+            return None
+
+        def save_response_content(response, destination):
+            CHUNK_SIZE = 32768
+
+            with open(destination, "wb") as f:
+                for chunk in response.iter_content(CHUNK_SIZE):
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+        file_id = '1W2-4Itl4nJvG4PSl0ZYhPJZv3MSz550G'
+        destination = dataset_path + video_folder +"youtube.mp4"
+        download_file_from_google_drive(file_id, destination)
+        #video='https://drive.google.com/file/d/1W2-4Itl4nJvG4PSl0ZYhPJZv3MSz550G/view?usp=sharing'
+        #video.streams.get_by_itag(18).download(output_path = dataset_path + video_folder, filename="youtube.mp4")
         fe = FrameExtractor(dataset_path + video_folder + "/youtube.mp4")
         print(fe.n_frames)
         print(fe.get_video_duration())
